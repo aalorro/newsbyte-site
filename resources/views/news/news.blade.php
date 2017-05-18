7f72fd1a756ff5eab5a0158ebe5d75097cd2947c
elocin <nicole.alday1111@gmail.com>
2017-05-18 18:13:53 +0800

    articles list thumbnails

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -41,34 +41,42 @@
                         }
 
                     ?>
-                    <article class="@if($currentLang == 'Arabic' || $currentLang == 'Urdu' || $native == 'Dhivehi'){{ 'text-right' }} @else {{ '' }} @endif">
-                        @if ($val['thumbnail'] != '')
-                            <img src="{{ $val['thumbnail'] }}" class="thumbnail" width="200px">
-                        @endif
-                        <p >
-                            <a href="{{url('/article')}}/{{ $region }}/{{ $val['country'] }}/{{ $val['item_id'] }}/{{ $val['language'] }}" class="a_link">
-                                {{ $val['title'] }}
-                            </a>
-                        </p>
-                        <ul class="list-inline">
-                            <li class="news_source">
-                                <?php 
-                                    $url = parse_url($val['source']); 
-                                        $host = str_replace('www.', '', $url['host']);
-                                    if($host == 'feeds.feedburner.com'){
-                                        $id = substr($val['source'], strrpos($val['source'], '/') + 1);
-                                            $source = explode('?', $id);
-                                        echo $source[0];
-                                    }else{
-                                       echo $host;
-                                    }                                
-                                ?>
-                            </li>
-                            <li class="pull-right">
-                               <?php echo $val['pubdate']; ?>
-                            </li>
-                        </ul>
-                        <p>{!! $val['description'] !!}</p>
+                    <article class="@if($currentLang == 'Arabic' || $currentLang == 'Urdu' || $native == 'Dhivehi'){{ 'text-right' }} @else {{ '' }} @endif" style="padding:20px 0;">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>
+                                <a href="{{url('/article')}}/{{ $region }}/{{ $val['country'] }}/{{ $val['item_id'] }}/{{ $val['language'] }}" class="a_link">
+                                    {{ $val['title'] }}
+                                </a>
+                            </p>
+                        </div>
+                        <div class="col-md-3">
+                            @if ($val['thumbnail'] != '')
+                                <img src="{{ $val['thumbnail'] }}" class="thumbnail img-responsive">
+                            @endif
+                        </div>
+                        <div class="col-md-9">
+                            <ul class="list-inline">
+                                <li class="news_source">
+                                    <?php 
+                                        $url = parse_url($val['source']); 
+                                            $host = str_replace('www.', '', $url['host']);
+                                        if($host == 'feeds.feedburner.com'){
+                                            $id = substr($val['source'], strrpos($val['source'], '/') + 1);
+                                                $source = explode('?', $id);
+                                            echo $source[0];
+                                        }else{
+                                           echo $host;
+                                        }                                
+                                    ?>
+                                </li>
+                                <li class="pull-right">
+                                   <?php echo $val['pubdate']; ?>
+                                </li>
+                            </ul>
+                            <p>{!! $val['description'] !!}</p>
+                        </div>
+                    </div>
                     </article>
                 @endforeach
             </div>
